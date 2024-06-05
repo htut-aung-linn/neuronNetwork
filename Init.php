8e33c19ddc7c7900b9684409048ee1d358e67aef
Htut Aung Linn <123470908+htut-aung-linn@users.noreply.github.com>
2024-06-05 00:04:08 -0700

    Update Init.php

--- Init.php
+++ Init.php
@@ -13,7 +13,7 @@ for($j=0;$j<1;$j++){
 for($j=0;$j<1;$j++){
     for($i=0;$i<count($X);$i++){
         $myNet ->train($X[$i],$y[$i],0.1);
-    }
+    }}
     $filename = 'data.txt';
     // The content to write to the file
     $content = $myNet->getWeight();
